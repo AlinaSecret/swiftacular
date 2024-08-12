a32d51fbac7b14d690c55ba963288bf9320f607e
Alina Sudakov <asudakov@redhat.com>
2024-08-12 20:13:48 +0300

    Unknown or illegal instance identifier

--- swift/swiftdb.py
+++ swift/swiftdb.py
@@ -2,29 +2,28 @@ from swift.container.backend import ContainerBroker
 
 import subprocess
 
+def get_dbs():
+    command = [
+        'find', '/', '-type', 'f',
+        '(', '-name', '*.sqlite', '-o', '-name', '*.db', '-o', '-name', '*.sqlite3', ')'
+    ]
+    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+    stdout, stderr = process.communicate()
+    if process.returncode != 0:
+        print()
+    return stdout.splitlines()
+
+# # Iterate over each line in the output
+# for line in stdout.splitlines():
+#     if "containers" in line:
+#         c = ContainerBroker(line)
+#         d = c.get_info()
+#         print(d["bytes_used"])
 
 
-command = [
-     'find', '/', '-type', 'f',
-    '(', '-name', '*.sqlite', '-o', '-name', '*.db', '-o', '-name', '*.sqlite3', ')'
-]
+# print()
+import pathlib
 
-# Run the command
-process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-
-# Capture the output and errors
-stdout, stderr = process.communicate()
-
-# Check for errors
-if process.returncode != 0:
-    print()
-
-# Iterate over each line in the output
-for line in stdout.splitlines():
-    if "containers" in line:
-        c = ContainerBroker(line)
-        d = c.get_info()
-        print(d["bytes_used"])
-
-
+root = pathlib.Path("/home")
+a = list(root.rglob("*containers*.db"))
 print()
